e9cc5baae88607fde9da2989ad12689e154b293e
Rhuan Carlos Vieira Feitoza <rhuanfeitoza1@gmail.com>
2026-07-31 15:27:25 -0300

    chore: configure Netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
 
 export default defineConfig({
   output: "server",
-  adapter: cloudflare({
-    imageService: "compile",
-  }),
+  adapter: netlify(),
   integrations: [react()],
   prefetch: {
     prefetchAll: true,
